1652cd7f031de69551225d645a96511e4492eae2
Apurva <apurvanaik2024@gmail.com>
2024-08-26 16:16:54 +0530

    Added Test1 file

---
+++ src/test/java/TestComponents/Test1.java
@@ -0,0 +1,14 @@
+package TestComponents;
+
+public class Test1 {
+
+    public void add(int a,int b){
+        int c =a+b;
+
+    }
+
+    public void diff(int a,int b){
+        int c =a-b;
+
+    }
+}
