89349bc4b3fe96113f90961627495b815c21c92a
Apurva <apurvanaik2024@gmail.com>
2024-08-26 16:23:56 +0530

    Updated Test1 file

--- src/test/java/TestComponents/Test1.java
+++ src/test/java/TestComponents/Test1.java
@@ -2,13 +2,15 @@ package TestComponents;
 
 public class Test1 {
 
-    public void add(int a,int b){
+    public int add(int a,int b){
         int c =a+b;
+        return c;
 
     }
 
-    public void diff(int a,int b){
+    public int diff(int a,int b){
         int c =a-b;
+        return c;
 
     }
 }
